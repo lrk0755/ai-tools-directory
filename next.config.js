7b7e1b5036bc7a782d76c04332ad3277f33b4bc8
Renkai <renkai@example.com>
2026-03-16 22:58:12 +0800

    Configure static export for Cloudflare Pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
+  output: 'export',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
